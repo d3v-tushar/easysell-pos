7a0fcde357f628572a31a48f0a6fb0b92293316b
RN1216 <reunnoor@gmail.com>
2023-04-18 01:17:49 +0600

    Ädd paymeny layout

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
-            
+             <header className="sticky top-0 bg-white z-50">
+        <h2 className="text-4xl font-bold leading-7 p-6 text-gray-600">
+          Make a Payment
+        </h2>
+      </header>
         </div>
     );
 };
